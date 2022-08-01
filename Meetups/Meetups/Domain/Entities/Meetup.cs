8f36b904ae6a9c1fb1fb03848b1a4649b9e8f751
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-08-01 11:10:54 +0300

    Create project with Meetup entity

---
+++ Meetups/Meetups/Domain/Entities/Meetup.cs
@@ -0,0 +1,14 @@
+﻿namespace Meetups.Domain.Entities
+{
+    public class Meetup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty; 
+        public string Plan { get; set; } = string.Empty;
+        public string Speeker { get; set; } = string.Empty;
+        public string Organizer { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+    }
+}
